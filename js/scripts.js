5211dc11ac462ed9ea99f1d634fcf8a68e11de80
Benjamin Logue and Matthew Giem <matthew@>
2016-05-31 15:30:05 -0700

    simplified function to toggle children in div to show and hide children elements.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 var main = function() {
 
-  function handler( event ) {
-  var target = $( event.target );
-  if ( target.is( ".cards" ) ) {
-    target.children().toggle();
-  }
-}
-$( ".cards" ).click( handler ).find( "p" ).hide();
+  $(".cards").click(function(){
+    $(this).children().toggle();
+
+  });
+
 }
 
 
